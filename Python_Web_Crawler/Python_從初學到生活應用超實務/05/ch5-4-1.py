f39f1bec95dd9aa2adbe75122c6786af4a6bf9cb
hehank <46835548+hehank@users.noreply.github.com>
2022-01-21 02:04:18 +0800

    study

--- Python_Web_Crawler/Python_從初學到生活應用超實務/05/ch5-4-1.py
+++ Python_Web_Crawler/Python_從初學到生活應用超實務/05/ch5-4-1.py
@@ -1,2 +1,19 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://fchart.github.io"
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    tags = soup("a")
+
+    for tag in tags:
+        href = tag.get("href", None)
+        print(href)
+
+else:
+    print("HTTP Request Rrror")
